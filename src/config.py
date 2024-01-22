6b01368d5f17eecb12c7a39cce1ef2efe3d4a193
ivanprofpv <akvabanka@gmail.com>
2024-01-22 10:31:07 +0400

    added base settings tests and override dependes

--- src/config.py
+++ src/config.py
@@ -12,3 +12,10 @@ SMTP_USER = os.environ.get("SMTP_USER")
 SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
 
 SECRET_AUTH = os.environ.get("SECRET_AUTH")
+
+
+DB_HOST_TEST = os.environ.get("DB_HOST_TEST")
+DB_NAME_TEST = os.environ.get("DB_NAME_TEST")
+DB_USER_TEST = os.environ.get("DB_USER_TEST")
+DB_PASS_TEST = os.environ.get("DB_PASS_TEST")
+DB_PORT_TEST = os.environ.get("DB_PORT_TEST")
